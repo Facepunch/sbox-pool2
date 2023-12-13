f5c37c84ded78959d448dafec2feb4f2ef32c4b3
kurozael <kurozael@gmail.com>
2023-12-13 23:12:14 +0000

    Random force component test + add rigidbody to the model collider objects

---
+++ code/ApplyRandomForce.cs
@@ -0,0 +1,12 @@
+﻿namespace Sandbox;
+
+public class ApplyRandomForce : Component
+{
+	protected override void OnStart()
+	{
+		var comp = Components.Get<Rigidbody>();
+		comp.ApplyForce( Vector3.Random * 1000f );
+		
+		base.OnStart();
+	}
+}
