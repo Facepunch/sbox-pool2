5e6b70089ee4d12295c84b75b883ef0ccb7760a1
kurozael <kurozael@gmail.com>
2023-12-14 12:19:35 +0000

    PoolBallSpawn uses Gizmo to render in editor - add hitbox, set material group etc.

--- code/PoolBallSpawn.cs
+++ code/PoolBallSpawn.cs
@@ -2,36 +2,40 @@
 
 namespace Facepunch.Pool;
 
-public class PoolBallSpawn : Component, Component.ExecuteInEditor
+public class PoolBallSpawn : Component
 {
 	[Property] public PoolBallType Type { get; set; }
 	[Property] public PoolBallNumber Number { get; set; }
 
 	protected override void DrawGizmos()
 	{
-		base.DrawGizmos();
-	}
+		var model = Model.Load( "models/pool/pool_ball.vmdl" );
+		Gizmo.Hitbox.Model( model );
 
-	protected override void OnUpdate()
-	{
-		var renderer = Components.Get<ModelRenderer>();
-		if ( renderer is not null )
-		{
-			renderer.MaterialGroup = GetMaterialGroup();
-		}
+		if ( Gizmo.IsSelected )
+			Gizmo.Draw.Color = Color.White.WithAlpha( 0.3f );
+		else if ( Gizmo.IsHovered )
+			Gizmo.Draw.Color = Color.White.WithAlpha( 0.1f );
 		
-		base.OnUpdate();
+		if ( Gizmo.IsHovered || Gizmo.IsSelected )
+			Gizmo.Draw.LineSphere( Vector3.Zero, 1.15f, 10 );
+		
+		Gizmo.Draw.Color = Color.White;
+		
+		var so = Gizmo.Draw.Model( model, global::Transform.Zero );
+		so.SetMaterialGroup( GetMaterialGroup() );
+		
+		base.DrawGizmos();
 	}
 
 	private string GetMaterialGroup()
 	{
-		if ( Type == PoolBallType.Black )
-			return "8";
-		else if ( Type == PoolBallType.Spots )
-			return ((int)Number).ToString();
-		else if ( Type == PoolBallType.Stripes )
-			return ((int)Number + 8).ToString();
-
-		return "default";
+		return Type switch
+		{
+			PoolBallType.Black => "8",
+			PoolBallType.Spots => ((int)Number).ToString(),
+			PoolBallType.Stripes => ((int)Number + 8).ToString(),
+			_ => "default"
+		};
 	}
 }
